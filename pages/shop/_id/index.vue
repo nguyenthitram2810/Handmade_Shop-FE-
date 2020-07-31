bbb3f61063573282f35d4a8b2651820ade8bbe8d
nguyenthitram2810 <nguyenthitram2010a@gmail.com>
2020-07-31 14:49:34 +0700

    view shop

--- pages/shop/_id/index.vue
+++ pages/shop/_id/index.vue
@@ -27,7 +27,7 @@
 						<div class="col-lg-2 col-md-3">
 							<div class="profile-author">
 								<div class="profile-author-thumb">
-									<img alt="author" :src="shop.thumbnail">
+									<img alt="author" :src="user.shop.thumbnail">
 									<div class="edit-dp">
 										<label class="fileContainer">
 											<i class="fa fa-camera"></i>
@@ -37,7 +37,7 @@
 								</div>
 									
 								<div class="author-content">
-									<a class="h4 author-name" href="about.html">{{ shop.name }}</a>
+									<a class="h4 author-name" href="about.html">{{ user.shop.name }}</a>
 								</div>
 							</div>
 						</div>
@@ -73,20 +73,19 @@
       <div class="row pl-2">
         <div class="col-lg-9 col-md-12"> 
           <div class="row shop_wrapper">
-            <div v-for="product in shop.products" :key="product.id" class="col-lg-3 col-md-3 col-12 ">
+            <div v-for="product in products" :key="product.id" class="col-lg-3 col-md-3 col-12 ">
               <article class="single_product">
                 <figure>
                   <div class="product_thumb">
-                    <a class="primary_img" href="product-details.html"><img :src="product.thumbnail" width="100%" alt=""></a>
+                    <a class="primary_img" href="product-details.html"><img src="http://res.cloudinary.com/dhbk/image/upload/v1596178101/gallery/io2wmnkohk47x7iiizcj.jpg" width="100%" alt=""></a>
                     <div class="label_product">
                         <span class="label_sale">-7%</span>
                     </div>
 
                     <div class="action_links">
                         <ul>
-                            <li class="add_to_cart"><a href="cart.html" title="Add to cart"><i class="icon-shopping-bag"></i></a></li>
-                            <li class="compare"><a href="#" title="Add to Compare"><i class="icon-sliders"></i></a></li>
-                             <li class="wishlist"><a href="wishlist.html" title="Add to Wishlist"><i class="icon-heart"></i></a></li>    
+                            <li class="add_to_cart"><a href="#" title="Add to cart"><i class="icon-shopping-bag"></i></a></li>
+                             <li class="wishlist"><a href="#" title="Add to Wishlist"><i class="icon-heart"></i></a></li>    
                             <li class="quick_button"><a href="#" data-toggle="modal" data-target="#modal_box"  title="quick view"> <i class="icon-eye"></i></a></li>
                         </ul>
                     </div>
@@ -110,36 +109,9 @@
                         </div>
                         <h4 class="product_name"><a href="product-details.html">{{product.name}}</a></h4>
                         <div class="price_box"> 
-                          <span class="current_price">£69.00</span>
-                          <span class="old_price">£74.00</span>
+                          <span class="current_price">₫ {{product.price}}</span>
+                          <!-- <span class="old_price">£74.00</span> -->
                         </div>
-                      </div>
-                    </div>
-                    <div class="product_content list_content">
-                      <div class="product_rating">
-                        <ul>
-                            <li><a href="#"><i class="icon-star"></i></a></li>
-                            <li><a href="#"><i class="icon-star"></i></a></li>
-                            <li><a href="#"><i class="icon-star"></i></a></li>
-                            <li><a href="#"><i class="icon-star"></i></a></li>
-                            <li><a href="#"><i class="icon-star"></i></a></li>
-                        </ul>
-                      </div>
-
-                      <h4 class="product_name"><a href="product-details.html">{{product.name}}</a></h4>
-                      <div class="price_box"> 
-                        <span class="current_price">₫{{product.price}}</span>
-                        <!-- <span class="old_price">£74.00</span> -->
-                      </div>
-                      <div class="product_desc">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco…</p>
-                      </div>
-                      <div class="action_links list_action_right">
-                        <ul>
-                          <li class="add_to_cart"><a href="cart.html" title="Add to cart">Add to cart</a></li>
-                          <li class="wishlist"><a href="wishlist.html" title="Add to Wishlist"><i class="icon-heart"></i></a></li>   
-                          <li class="compare"><a href="#" title="Add to Compare"><i class="icon-sliders"></i></a></li> 
-                        </ul>
                       </div>
                     </div>
                   </figure>
@@ -157,30 +129,15 @@
               <a-layout-sider width="100%" style="background: #fff">
                 <a-menu
                   mode="inline"
-                  :default-selected-keys="['1']"
+                  :default-selected-keys="['0']"
                   :style="{ height: '100%', borderRight: 0 }"
                 >
-                  <a-menu-item key="1">
+                  <a-menu-item key="0">
                     Tất cả sản phẩm
                   </a-menu-item>
-                  <a-sub-menu key="sub1">
-                    <span slot="title">Áo quần</span>
-                    <a-menu-item key="2">
-                      Áo quần nam
-                    </a-menu-item>
-                    <a-menu-item key="3">
-                      Áo quần nữ
-                    </a-menu-item>
-                  </a-sub-menu>
-                  <a-sub-menu key="sub2">
-                    <span slot="title">Giày dép</span>
-                    <a-menu-item key="4">
-                      Giày Custom
-                    </a-menu-item>
-                    <a-menu-item key="5">
-                      Dép Custom
-                    </a-menu-item>
-                  </a-sub-menu>
+                  <a-menu-item v-for="(cate, index) in listCate" :key="index + 1">
+                    {{cate.name}}
+                  </a-menu-item>
                 </a-menu>
               </a-layout-sider>
             </a-layout>
@@ -194,6 +151,7 @@
 <script>
 import Product from '~/components/Product'
 const Cookie = process.client ? require('js-cookie') : undefined
+import axios from "axios"
 
 export default {
   components: {
@@ -201,35 +159,51 @@ export default {
   },
   data() {
     return {
-      shop: {
-        id: 1,
-        thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTfTK0N0iseFntGW7CWT4e0qcpE7eqj2ZBKIw&usqp=CAU',
-        description: 'Shop thời trang cute',
-        name: 'SHT_Life',
-        slug: 'sht_life',
-        products: [
-          {
-            id: 1,
-            name: 'Áo pull J2TEAM',
-            thumbnail: 'http://res.cloudinary.com/dhbk/image/upload/v1596131648/gallery/tvgb1fqmpkmm7mmxismz.jpg',
-            price: "120000"
-          },
-          {
-            id: 2,
-            name: 'Quần baggy',
-            thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTfTK0N0iseFntGW7CWT4e0qcpE7eqj2ZBKIw&usqp=CAU',
-            price: "250000"
-          }
-        ]
-      }
+      user: JSON.parse(Cookie.get('user')),
+      products: [],
+      listCate: [],
     }
   },
   mounted() {
     this.getInfoShop()
   },
   methods: {
-    getInfoShop() {
-      console.log(this.$route.params.id);
+    async getInfoShop() {
+      try {
+        const response = await axios.get(`http://localhost:5000/api/v1/shop/${this.$route.params.id}/products`)
+        console.log(response);
+        if(response.data.status == "200") {
+          this.products = response.data.data
+          this.getListCate(response.data.data)
+        }
+        else {
+          this.$notification["error"]({
+            message: 'GET PRODUCT ERROR',
+            description:
+              response.data.message
+          });
+        }
+      }
+      catch(e) {
+        this.$notification["error"]({
+          message: 'GET PRODUCT ERROR',
+          description:
+            response.data.message
+        });
+      }
+    },
+
+    getListCate(data) {
+      let temp = new Set()
+      data.forEach(e => {
+        let i = temp.size
+        console.log(i);
+        temp.add(e.category.id)
+        if(i < temp.size) {
+          this.listCate.push(e.category)
+        }
+      });
+      console.log(this.listCate);
     }
   }
 }
