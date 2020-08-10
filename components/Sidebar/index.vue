068cbc8998a3e977b8443d5f84fb9343f8aa4c04
dangphu2412 <dangphu241299@gmail.com>
2020-08-10 19:00:56 +0700

    Init base admin

---
+++ components/Sidebar/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <a-layout-sider v-model="collapsed" :trigger="null" collapsible>
+      <div class="logo" />
+        <a-menu theme="dark" mode="inline" :default-selected-keys="['sub1']">
+          <a-sub-menu key="sub1">
+            <span slot="title"><a-icon type="user" /><span>User</span></span>
+              <a-menu-item key="3">
+                Tom
+              </a-menu-item>
+              <a-menu-item key="4">
+                Bill
+              </a-menu-item>
+              <a-menu-item key="5">
+                Alex
+              </a-menu-item>
+          </a-sub-menu>
+        </a-menu>
+    </a-layout-sider>
+</template>
+
+<script>
+export default {
+  props: ["collapsed"]
+}
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+}
+</style>
---
+++ layouts/admin.vue
@@ -0,0 +1,29 @@
+<template>
+  <a-layout>
+    <SidebarAdmin :collapsed="collapsed"/>
+    <AdminHeader :collapsed="collapsed" @toggleSidebar="collapsed = !collapsed">
+      <nuxt/>
+    </AdminHeader>
+    <BaseFooter/>
+  </a-layout>
+</template>
+
+<script>
+import SidebarAdmin from '@/components/Sidebar/index';
+import AdminHeader from '@/components/Header/admin';
+export default {
+  components: {
+    SidebarAdmin,
+    AdminHeader,
+  },
+  data() {
+    return {
+      collapsed: false,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
